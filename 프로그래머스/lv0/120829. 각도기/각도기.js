8ff90f624ec5f1cca3c689f1105f45371f20f0f8
sojin jang <138555977+sojinkorea@users.noreply.github.com>
2023-08-03 22:44:16 +0900

    [level 0] Title: 각도기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -4,7 +4,7 @@ function solution(angle) {
         answer = 1;
     } else if (angle == 90) {
         answer = 2;
-    } else if (90 < angle && angle < 180) {
+    } else if (angle < 180) {
         answer = 3;
     } else {
         answer = 4;
