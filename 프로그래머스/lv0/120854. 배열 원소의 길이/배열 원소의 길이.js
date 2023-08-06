2ac4c08873e3296891ec60cb21e58c66a1cb92c9
sojin jang <138555977+sojinkorea@users.noreply.github.com>
2023-08-06 18:33:01 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,7 +1,3 @@
 function solution(strlist) {
-    var answer = [];
-    for (let i = 0 ; i < strlist.length ; i++) {
-        answer[i] = strlist[i].length;
-    }
-    return answer;
+    return strlist.map((el) => el.length);
 }
