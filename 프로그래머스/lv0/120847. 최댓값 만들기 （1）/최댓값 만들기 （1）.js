7bc2461736e77fe40021e6173706824297ee2d10
sojin jang <138555977+sojinkorea@users.noreply.github.com>
2023-08-07 17:39:58 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,4 +1,11 @@
+// function solution(numbers) {
+//     let arr = numbers.sort((a, b) => b - a);
+//     return arr[0] * arr[1];
+// }
+
+//sort는 원본 배열이 정렬된다.
+
 function solution(numbers) {
-    let arr = numbers.sort((a, b) => b - a);
-    return arr[0] * arr[1];
+    numbers.sort((a, b) => b - a);
+    return numbers[0] * numbers[1];
 }
