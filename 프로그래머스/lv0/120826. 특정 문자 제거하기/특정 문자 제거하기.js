eaf841aced67f64fa156c939859a44d4fbbc1e86
sojin jang <138555977+sojinkorea@users.noreply.github.com>
2023-08-08 20:32:02 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,6 +1,3 @@
 function solution(my_string, letter) {
-    return my_string
-        .split('')
-        .filter((v) => v !== letter)
-        .join('');
+    return my_string.replaceAll(letter,'');
 }
