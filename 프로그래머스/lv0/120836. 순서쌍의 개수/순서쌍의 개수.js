9428408eddb5512d60326e955b173e7c09d8107f
sojin jang <138555977+sojinkorea@users.noreply.github.com>
2023-08-09 21:46:19 +0900

    [level 0] Title: 순서쌍의 개수, Time: 5.37 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,9 +1,9 @@
 function solution(n) {
-    const answer = []
-    for (let i = 0; i < n + 1; i++) {
+    let answer = 0;
+    for (let i = 0; i <= n; i++) {
         if (n % i === 0) {
-            answer.push(i)
+            answer++;
         }
     }
-    return answer.length
+    return answer;
 }
