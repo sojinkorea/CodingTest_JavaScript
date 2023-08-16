cea318575ae684ac2ed297af3cceab4aaf0fb5ea
sojin jang <138555977+sojinkorea@users.noreply.github.com>
2023-08-16 23:36:31 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.27 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,11 +1,5 @@
 function solution(my_string) {
-    var answer = '';
-    for(i = 0; i < my_string.length; i++) {
-        if (my_string[i] === my_string[i].toUpperCase()) {
-            answer += my_string[i].toLowerCase();
-            } else {
-                answer += my_string[i].toUpperCase();
-            }
-    }
-    return answer;
+    return [...my_string]
+        .map(v => v == v.toUpperCase() ? v.toLowerCase() : v.toUpperCase())
+        .join('')
 }
