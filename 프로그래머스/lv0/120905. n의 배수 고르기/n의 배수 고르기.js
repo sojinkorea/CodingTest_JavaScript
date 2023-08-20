bf4e172544abf9c762bea5e1f38ba45c0a4979e8
sojin jang <138555977+sojinkorea@users.noreply.github.com>
2023-08-20 16:53:04 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,9 +1,3 @@
 function solution(n, numlist) {
-    for(let i = 0; i < numlist.length; i++) {
-        if(numlist[i] % n !== 0) {
-            numlist.splice(i, 1);
-            i--;
-        }
-    }
-    return numlist;
+    return numlist.filter(item => item % n === 0);
 }
