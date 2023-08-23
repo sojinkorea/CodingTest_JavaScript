2ad6b7cf5ef19d31093bd91356601fc2a90c235f
sojin jang <138555977+sojinkorea@users.noreply.github.com>
2023-08-23 17:04:00 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,3 +1,4 @@
 function solution(box, n) {
-    return box.map(v => Math.floor(v / n)).reduce((a, c) => a * c);
+    // return box.map(v => Math.floor(v / n)).reduce((a, c) => a * c);
+    return box.reduce((a, c) => a * Math.floor(c / n), 1);
 }
