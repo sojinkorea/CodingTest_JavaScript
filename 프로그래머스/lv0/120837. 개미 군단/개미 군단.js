b4c6ea9603beb414640fb29810e83a516fb07e59
sojin jang <138555977+sojinkorea@users.noreply.github.com>
2023-08-26 18:23:39 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,9 +1,3 @@
 function solution(hp) {
-    if (hp % 5 === 0) {
-        return hp / 5;
-    } else if ((hp % 5) % 3 === 0) {
-        return Math.floor(hp / 5) + ((hp % 5) / 3);
-    } else {
         return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + ((hp % 5) % 3)
-    }
 }
