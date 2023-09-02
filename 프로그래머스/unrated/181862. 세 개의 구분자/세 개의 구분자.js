7bbd46915669fc13faac1a4ea426340dd382f783
sojin jang <138555977+sojinkorea@users.noreply.github.com>
2023-09-02 23:04:13 +0900

    [unrated] Title: 세 개의 구분자, Time: 16.12 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,4 +1,4 @@
 function solution(myStr) {
-    const array = myStr.split(/[a|b|c]/).filter(v => v);
+    const array = myStr.split(/[abc]/g).filter(v => v);
     return array.length ? array : ["EMPTY"]
 }
